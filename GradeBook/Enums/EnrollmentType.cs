0acd92e3aa8cd327021ba952214554dfb464195e
Michał Wojciechowski <michal.wojciechowsk3@microsoft.wsei.edu.pl>
2025-04-12 17:10:54 +0200

    Dodaj pliki projektów.

---
+++ GradeBook/Enums/EnrollmentType.cs
@@ -0,0 +1,22 @@
+﻿namespace GradeBooks.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+    public class BaseGradeBook
+    {
+        
+        public GradeBookType Type { get; set; }
+
+        public BaseGradeBook(GradeBookType type)
+        {
+            Type = type;
+        }
+
+    }
+}
